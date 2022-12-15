482bf6ed8e40d03262d1e2ddefb480d24bd3ead5
surprise <viktar.yeustsihneyeu@instinctools.com>
2022-12-16 00:21:40 +0300

    init

--- src/containers/Teachers/data.js
+++ src/containers/Teachers/data.js
@@ -1,12 +1,12 @@
 import teach1 from "../../img/teach1.jpg";
 import teach2 from "../../img/teach2.jpg";
 import teach3 from "../../img/teach3.jpg";
-import teach4 from "../../img/teach4.jpg";
+// import teach4 from "../../img/teach4.jpg";
 
 import teach1Thumb from "../../img/teach1-thumb.png";
 import teach2Thumb from "../../img/teach2-thubm.png";
 import teach3Thumb from "../../img/teach3-thumb.png";
-import teach4Thumb from "../../img/teach4-thumb.png";
+// import teach4Thumb from "../../img/teach4-thumb.png";
 
 export const TEACH_ARR = [
   {
@@ -19,16 +19,16 @@ export const TEACH_ARR = [
     description:
       "Дипломированный инженер-программист. Изучаю компьютерные технологии и современные платформы программирования. Очень люблю работать с детьми, делиться с ними знаниями. Считаю, что программирование - навык, необходимый каждому образованному человеку. В свободное время люблю изучать что-то новое, гулять по городу, играть в настольные игры.",
   },
-  {
-    thumbnail: teach4Thumb,
-    image: teach4,
-    name: "Ляпин Дмитрий Васильевич ",
-    tel: "+375298991165",
-    spec: "React",
-    city: "Брест",
-    description:
-      "Преподаю следующие направления (в том числе для детей от 7 до 17 лет): - Создание сайтов (HTML, CSS, JavaScript) 10+ - Программирование на C++ 12+ - Программирование на Python 10+ - Программирование на C# 12+ - Создание 3D-игр в Roblox Studio 9+ - Программирование 2d и 3d игр в Unity 12+ - Программирование на Scratch 7+",
-  },
+  // {
+  //   thumbnail: teach4Thumb,
+  //   image: teach4,
+  //   name: "Ляпин Дмитрий Васильевич ",
+  //   tel: "+375298991165",
+  //   spec: "React",
+  //   city: "Брест",
+  //   description:
+  //     "Преподаю следующие направления (в том числе для детей от 7 до 17 лет): - Создание сайтов (HTML, CSS, JavaScript) 10+ - Программирование на C++ 12+ - Программирование на Python 10+ - Программирование на C# 12+ - Создание 3D-игр в Roblox Studio 9+ - Программирование 2d и 3d игр в Unity 12+ - Программирование на Scratch 7+",
+  // },
   {
     thumbnail: teach1Thumb,
     image: teach1,
